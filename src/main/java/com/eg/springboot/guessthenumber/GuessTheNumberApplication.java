300eaaf8a90eb0cdbfba2ff714e40f06bc69adca
Elen4538 <mpl4538@gmail.com>
2023-03-24 19:19:32 +0000

    First commit.Create the project necessary configurations

---
+++ src/main/java/com/eg/springboot/guessthenumber/GuessTheNumberApplication.java
@@ -0,0 +1,19 @@
+package com.eg.springboot.guessthenumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessTheNumberApplication {
+
+	public static void main(String[] args) {
+
+
+
+		SpringApplication.run(GuessTheNumberApplication.class, args);
+
+
+		//System.out.println("guess the number ");
+	}
+
+}
